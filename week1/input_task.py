1c6c9ef934429c1a8e317f31b36f5135405cc5eb
Muhammad Yameen Raza <2razam32@solent.ac.uk>
2023-12-08 02:31:22 +0000

    example

--- week1/input_task.py
+++ week1/input_task.py
@@ -1,4 +1,2 @@
-# Ask user to enter their name
-print("what is your name?")
-name = input()
-print(f"It is nice to meet you {name}")
+name = "John smith"
+
---
+++ week1/new.py
@@ -0,0 +1,2 @@
+name = input("Please Enter your name: ")
+if name == "John"
